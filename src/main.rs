ae4fe71c6e5984b78bb554d1c5bebc264816b747
Hydrogen <3038094028@qq.com>
2022-12-10 21:04:56 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 pub mod v1;
 
 use crate::v1::resolver::file::FileId;
-use sevenz_rust::{SevenZReader, default_entry_extract_fn};
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -9,23 +8,5 @@ async fn main() -> Result<(), anyhow::Error> {
 
     println!("{:#?}", id);
 
-    Ok(())
-}
-
-async fn extract(id: FileId) -> Result<(), anyhow::Error> {
-    let mut a = SevenZReader::open(&id, &[])?;
-
-    let mut t = vec![];
-
-    a.for_each_entries(move |a, b| {
-        t.push(extract_one());
-        Ok(true)
-    });
-
-    Ok(())
-}
-
-
-async fn extract_one() -> Result<(), anyhow::Error> {
     Ok(())
 }
