909e911c791823415267cc5d9eb496cd1230cada
Hydrogen <3038094028@qq.com>
2022-12-11 13:22:45 +0800

    chore: sync

--- src/v1/loader/mod.rs
+++ src/v1/loader/mod.rs
@@ -6,20 +6,24 @@ use crate::shared::archive::extract_async;
 
 use super::{resolver::package::PackageId, discovery::core::CoreDescriptor};
 
-pub struct Loader;
+pub struct Loader<'a> {
+  core: &'a CoreDescriptor,
+  pkg: &'a PackageId
+}
 
-impl Loader {
-  pub async fn load(&self, core: &CoreDescriptor, pkg: &PackageId) -> Result<(), anyhow::Error> {
+impl<'a> Loader<'a> {
+  pub async fn load(&self) -> Result<(), anyhow::Error> {
     Ok(())
   }
 
-  pub async fn extract(&self, core: &CoreDescriptor, pkg: &PackageId) -> Result<(), anyhow::Error> {
-    extract_async(&pkg, &core.workdir).await?;
+  pub async fn extract(&self) -> Result<(), anyhow::Error> {
+    extract_async(&self.pkg, &self.core.workdir).await?;
 
     Ok(())
   }
 
 
+  #[inline]
   pub fn is_script<P: AsRef<Path>>(&self, path: P) -> bool {
     matches!(
       path.as_ref().extension().map(|v| v.to_str()).flatten(),
@@ -27,8 +31,8 @@ impl Loader {
     )
   }
 
-  pub async fn clean_scripts(&self, core: &CoreDescriptor) -> Result<(), anyhow::Error> {
-    for i in core.workdir.read_dir()? {
+  pub async fn clean_scripts(&self) -> Result<(), anyhow::Error> {
+    for i in self.core.workdir.read_dir()? {
       if let Ok(i) = i {
         if i.metadata()?.is_file() && self.is_script(i.path()) {
 
